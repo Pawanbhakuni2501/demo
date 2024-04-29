80ba02de6275f6622a05344655db2202b5c6aba4
Pawanbhakuni2501 <168416109+Pawanbhakuni2501@users.noreply.github.com>
2024-04-29 14:22:12 +0530

    Add files via upload

---
+++ pttn.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++){
+            cout<<"i";
+        }
+        cout<<" ";
+    }
+}
